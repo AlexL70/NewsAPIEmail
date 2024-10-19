69299d4aca74b71027adaefe9292be9f433cafe8
Alexander Levinson <alexander.levinson.70@gmail.com>
2024-10-19 16:10:06 +0300

    254. Student Project: Email the API Data

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import requests as req
-import os
+import os, email.mime.text as mimetext, email.mime.message as mimemessage
+from send_email import send_email
 
+SEARCH_STRING="israel"
+SEARCH_DATE="2024-10-18"
 NEWSAPI_KEY = os.environ.get("NEWSAPI_KEY")
-URL = f"https://newsapi.org/v2/everything?q=israel&from=2024-09-19&sortBy=publishedAt&apiKey={NEWSAPI_KEY}"
+URL = f"https://newsapi.org/v2/everything?q={SEARCH_STRING}&from={SEARCH_DATE}&sortBy=publishedAt&apiKey={NEWSAPI_KEY}"
 
 r = req.get(URL)
 content = r.json()
-print(content['status'])
-print(content['totalResults'])
+if (content['status']) != "ok":
+    print(f"Error: {content['message']}")
+    exit(1)
+email_subject: str = f"News from newsapi.org for \"{SEARCH_STRING}\" on {SEARCH_DATE}"
+email_content: str = f""
 for article in content['articles']:
-    print(article['title'])
+    article_line: str = f"Title: <a href=\"{article['url']}\">{article['title']}</a><br>{article['description']}<br>By: {article['author']}"
+    email_content += f"{article_line}<br><br>"
+
+text = mimetext.MIMEText(email_content, "html")
+text["Subject"] = email_subject
+
+send_email(message=text.as_string().encode("utf-8"))
+
